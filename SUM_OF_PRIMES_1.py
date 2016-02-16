c95baed763b134265c3fb0bc044124bd7932777a
lfepr <lfepr@epr-uc.local>
2016-02-16 12:52:33 -0600

    -a

---
+++ SUM_OF_PRIMES_1.py
@@ -0,0 +1,29 @@
+## find the sum of the first n primes
+
+import time
+
+def isprime(arg): #is a number prime?
+    if arg >= 3:
+        for i in range(2,arg):
+            if (arg % i) == 0:
+                return False
+        return True
+    else:
+        if arg == 2:
+            return True
+        else:
+            return False
+
+def sum_primes():
+    Sum = 0
+    Prime_count = 0
+    loop = 1
+    while Prime_count<1000:
+        if isprime(loop):
+            Sum += loop
+            Prime_count += 1
+        loop += 1
+    return Sum
+
+Sum = sum_primes()
+print(Sum)
