cb2a87e5541cc6ad4772e101b7ed513b42f9586b
lfepr <lfepr@epr-uc.local>
2016-02-23 15:37:46 -0600

    added a medium challenge

---
+++ Medium_challenges/Mth_to_last_element.py
@@ -0,0 +1,4 @@
+#The first argument is a path to a file.
+#The file contains the series of space delimited characters followed by an integer.
+#The integer represents an index in the list (1-based), one per line.
+
